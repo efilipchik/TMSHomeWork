8cd4dedc12303518f2eacdb09a6f4114f134f301
efilipchik <egorberkut22-01@mail.ru>
2020-05-05 14:18:44 +0300

    Lesson13

---
+++ src/Lesson13/Runner.java
@@ -0,0 +1,7 @@
+package Lesson13;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(Validate.validate("src/lesson13/Devices.xsd","src/lesson13/Devices.xml"));
+    }
+}
